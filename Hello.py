8eef428886d29e51a453b7cbbf0ccb88e600ef84
Gabriel Beltrame SIlva <silvagabrielbeltrame@gmail.com>
2018-05-22 17:19:11 -0300

    Documentacao TKinter

--- Hello.py
+++ Hello.py
@@ -1,16 +1,30 @@
 import tkinter as tk
+import json
+
+from sinesp_client import SinespClient
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         self.create_widgets()
+        self.sinesp_procura(self.Et_placa)
 
     def create_widgets(self):
-        Label_placa = tk.Label(text='Placa:')
-        Label_placa.pack()
-        Entry_placa = tk.Label(text='XXX9999', bg='white')
-        Entry_placa.pack()
+        Et_placa = tk.Entry(bg='white')
+        Et_placa.pack()
+        return Et_placa
+
+    def sinesp_procura(self, placa):
+        # aqui dentro ele chama o escreve_widgets
+        vetor = []
+        sc = SinespClient()
+        results = sc.search(placa)
+        for result in results.values():
+           vetor.append(result)
+        for valor in vetor:
+            campo = tk.Label(text=vetor.pop())
+            campo.pack()
 
 root = tk.Tk()
 root.attributes("-topmost", True)

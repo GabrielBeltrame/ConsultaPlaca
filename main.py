835ca4ca10ebeb6593349ec1bcc277dae4e711c2
Gabriel Beltrame SIlva <silvagabrielbeltrame@gmail.com>
2018-05-23 19:27:34 -0300

    Funcionando tudo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Label, Button, Frame, Entry
+from tkinter import Tk, Label, Button, Frame, Entry, Listbox
+
 import json
 
 from sinesp_client import SinespClient
@@ -8,12 +9,14 @@ class Application(Frame):
         super().__init__(master)
         self.grid()
 
-        self.campo_placa = Entry(master)
+        self.campo_placa = Entry(master, width=15)
         self.campo_placa.grid()
 
         self.btn = Button(master, command=self.get_placa, text='->')
         self.btn.grid()
 
+        self.lstbox = Listbox(master, width=30, height=5)
+        self.lstbox.grid()
 
     def get_placa(self):
         self.placa = self.campo_placa.get()
@@ -26,13 +29,20 @@ class Application(Frame):
         results = sc.search(placa_sinesp)
         for result in results.values():
            vetor.append(result)
-        for valor in vetor:
-            campo = Label(text=vetor.pop())
-            campo.pack()
+            #fazer um foreach para deletar cada campo
+        self.lstbox.delete(0,5)
+        self.lstbox.insert(6, vetor[6])
+        self.lstbox.insert(7, vetor[7])
+        self.lstbox.insert(8, vetor[8])
+        self.lstbox.insert(12, vetor[12])
+        self.lstbox.insert(13, vetor[13])
+
+        self.lstbox.grid()
 
 root = Tk()
 root.attributes("-topmost", True)
 app = Application(master=root)
+app.master.geometry("200x200")
 app.master.title("Placa Sinesp")
 
 app.mainloop()
